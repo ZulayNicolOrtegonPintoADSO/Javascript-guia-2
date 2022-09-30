f0a4ad1ea5e8c76745e578c6ed67105f09237478
Zulay Ortegon <zulyortegon15@gmail.com>
2022-09-29 19:38:34 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 2. Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
-    
-    let num1 = Number(prompt("Número 1:"))
-    let num2= Number(prompt("Número 2:"))
-    let num3 = Number(prompt("Número 3:"))
+    // 3. Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o si son iguales.
+    mayor = 0
+    menor = 1000000
+    for(i = 0; i <= 2; i++){
+        let num = Number(prompt("Ingrese número:"))
+        if(num > mayor){
+            mayor = num
+        }
+        if(num < menor){
+            menor = num
+        }
 
-    if (num1 > num2 && num1 > num3){
-        console.log(`El número 1 es el mayor con un valor de: ${num1}`)
-    }
-    else if (num2 > num1 && num2 > num3){
-        console.log(`El número 2 es el mayor con un valor de: ${num2}`)
-    }
-    else if(num3 > num1 && num3 > num1){
-        console.log(`El número 3 es el mayor con un valor de: ${num3}`)
     }
 
-    else{
-        if(num1 == num2 && num1 == num3){
-            console.log(`Los números son de igual valor`)
-        }
-        else if(num1 == num2 && num1 > num3){
-            console.log(`Ay un empate de número mayor entre el num1 y num2 con un valor de${num1}`)
-        }
-        else if(num2 == num3 && num2 > num1){
-            console.log(`Ay un empate de número mayor entre el num2 y num3 con un valor de${num2}`)
-        }
-        else if(num1 == num3 && num1 > num2){
-            console.log(`Ay un empate de número mayor entre el num1 y num3 con un valor de${num1}`)}
-    }
+    console.log(`El número mayor es: ${mayor} , El número menor es: ${menor}`)
   })
