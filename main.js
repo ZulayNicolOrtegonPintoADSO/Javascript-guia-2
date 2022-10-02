cd58a7f5d21866711f227d21b1f8cc9e90a8f361
Zulay Ortegon <zulyortegon15@gmail.com>
2022-10-02 16:30:45 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 9. Realizar el algoritmo que lea N números, calcule y escriba la suma de los pares y el producto de los impares. Numero par es aquel que su residuo igual a cero (num mod !=0)
-    
-    pares = 0
-    impares =1
-    contador = 0 
-    cantidad = Number(prompt("Cantidad de números a añadir"))
-    while(contador < cantidad){
-        contador += 1
-        let num = Number(prompt("Número:"))
-        if(num %2==0){
-            console.log(`El número es par`)
-            pares += num
-        }
-
-        else{
-            console.log(`El número es impar`)
-            impares= impares * num
-            impares= impares 
-        }
+    //10. Calcular el valor total del valor de 5 productos, el IVA y el subtotal, visualizar los resultados de: Iva, Subtotal y Total de la compra de los artículos.
+    cont=0
+    subtotal=0
+    let iva = Number(prompt("Valor del iva en decimal: "))
+    let cantidad = Number(prompt("Cantidad de productos a añadir:"))
+    while(cont < cantidad){
+        cont += 1
+        let producto = Number(prompt("Valor del producto:"))
+        subtotal += producto
+        console.log(`El subtotal es ${subtotal}`)
     }
-    console.log(`La suma de los pares es ${pares} y el producto de los impares es ${impares}`)
+    console.log(`\n El valor en compra es ${subtotal} \n El iva es de ${iva} el total a pagar es ${subtotal + subtotal * iva}`)
   })
