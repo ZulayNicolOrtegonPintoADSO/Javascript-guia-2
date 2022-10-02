91c06149b49515521b4a50328e6702b6c60244c2
Zulay Ortegon <zulyortegon15@gmail.com>
2022-10-02 15:42:15 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 6. Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
-    let temperatura = Number(prompt("Ingrese valor para temperatura:"))
-    let presión = Number(prompt("Ingrese valor para presión:"))
-    if(temperatura > 100 | presión > 200){
-        console.log(`******************************ALERTA******************************************`)
+    // 7. Calcular todos los pagos hechos de un restaurante y que si el consumo ingresado excede los $130.000 el descuento será del 15%, de lo contrario no hay descuento.
+    subtotal=0
+    cont=0
+    let x = Number(prompt("Cantidad de pagos a añadir:"))
+    while(cont < x){
+        cont += 1
+        let pago = Number(prompt("valor pago:"))
+        subtotal += pago
+        console.log(`El pago es de ${pago} para un subtotal de ${subtotal}`)
     }
+
+    if(subtotal > 130000){
+        console.log(`\n Su compra tiene un valor de ${subtotal} \n como su compra excede los 130000 tiene un descuento de 15% \n El total a pagar es de ${subtotal - (subtotal*0.15)}`)
+    }
+
     else{
-        console.log(`_-_-_-_-_-_-_-_-NORMAL_-_-_-_-_-_-_-_-`)
+        console.log(`El total a pagar es ${subtotal}`)
     }
+
+
   })
