65a8df4b45495660b261c3d81f7b0f9c92a6b0c5
Zulay Ortegon <zulyortegon15@gmail.com>
2022-10-02 15:47:34 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 7. Calcular todos los pagos hechos de un restaurante y que si el consumo ingresado excede los $130.000 el descuento será del 15%, de lo contrario no hay descuento.
-    subtotal=0
-    cont=0
-    let x = Number(prompt("Cantidad de pagos a añadir:"))
-    while(cont < x){
-        cont += 1
-        let pago = Number(prompt("valor pago:"))
-        subtotal += pago
-        console.log(`El pago es de ${pago} para un subtotal de ${subtotal}`)
+    // 8. En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta, la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
+    let cargo = Number(prompt("Escoja a que cargo pertenece: \n 1. Planta \n 2. Administrativo"))
+    if(cargo == 1){
+        let horas = Number(prompt("Cantidad de horas trabajadas: "))
+        console.log(`El total a pagar es ${horas*20000}`)
     }
-
-    if(subtotal > 130000){
-        console.log(`\n Su compra tiene un valor de ${subtotal} \n como su compra excede los 130000 tiene un descuento de 15% \n El total a pagar es de ${subtotal - (subtotal*0.15)}`)
+    if(cargo == 2){
+        let horas = Number(prompt("Cantidad de horas trabajadas: "))
+        console.log(`El total a pagar es ${horas*10000}`)
     }
-
-    else{
-        console.log(`El total a pagar es ${subtotal}`)
-    }
-
-
   })
